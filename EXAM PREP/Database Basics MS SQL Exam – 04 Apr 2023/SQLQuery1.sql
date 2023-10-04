485088ed9a49c14fa59ce973cc61284bb934e7cb
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-10-04 23:25:49 +0100

    add queries

---
+++ EXAM PREP/Database Basics MS SQL Exam – 04 Apr 2023/SQLQuery1.sql
@@ -0,0 +1,88 @@
+CREATE TABLE Countries
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(10) NOT NULL
+)
+
+CREATE TABLE Addresses
+(
+	Id INT PRIMARY KEY IDENTITY,
+	StreetName VARCHAR(20) NOT NULL,
+	StreetNumber INT ,
+	PostCode INT NOT NULL,
+	City VARCHAR(25) NOT NULL,
+	CountryId INT FOREIGN KEY REFERENCES Countries(Id) NOT NULL
+)
+
+CREATE TABLE Vendors 
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(25) NOT NULL,
+	NumberVAT VARCHAR(15) NOT NULL,
+	AddressId INT FOREIGN KEY REFERENCES Addresses(Id) NOT NULL
+)
+
+CREATE TABLE Clients
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(25) NOT NULL,
+	NumberVAT VARCHAR(15) NOT NULL,
+	AddressId INT FOREIGN KEY REFERENCES Addresses(Id) NOT NULL
+)
+
+CREATE TABLE Categories
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(10) NOT NULL
+)
+
+CREATE TABLE Products
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(35) NOT NULL,
+	Price DEC NOT NULL,
+	CategoryId INT FOREIGN KEY REFERENCES Categories(Id) NOT NULL,
+	VendorId INT FOREIGN KEY REFERENCES Vendors(Id) NOT NULL
+)
+
+CREATE TABLE Invoices
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Number INT UNIQUE NOT NULL,
+	IssueDate DATETIME2 NOT NULL,
+	DueDate DATETIME2 NOT NULL,
+	Amount DEC NOT NULL,
+	Currency VARCHAR(5) NOT NULL,
+	ClientId INT FOREIGN KEY REFERENCES Clients(Id) NOT NULL
+)
+
+CREATE TABLE ProductsClients
+(
+	ProductId INT FOREIGN KEY REFERENCES Products(Id) NOT NULL,
+	ClientId INT FOREIGN KEY REFERENCES Clients(Id) NOT NULL
+	PRIMARY KEY(ProductId,ClientId)
+)
+
+INSERT INTO Products ([Name], Price, CategoryId, VendorId) VALUES 
+('SCANIA Oil Filter XD01', 78.69, 1, 1),
+('MAN Air Filter XD01', 97.38, 1, 5),
+('DAF Light Bulb 05FG87', 55.00, 2, 13),
+('ADR Shoes 47-47.5', 49.85, 3, 5),
+('Anti-slip pads S', 5.87, 5, 7)
+
+INSERT INTO Invoices (Number, Amount, IssueDate, DueDate, Currency, ClientId) VALUES
+(1219992181, 180.96, '2023-03-01', '2023-04-30', 'BGN', 3),
+(1729252340, 158.18, '2022-11-06', '2023-01-04', 'EUR', 13),
+(1950101013, 615.15, '2023-02-17', '2023-04-18', 'USD', 19)
+
+UPDATE Invoices
+SET DueDate = '2023-04-01'
+WHERE Year(IssueDate) = 2022 AND Month(IssueDate) = 11
+
+UPDATE Clients
+SET AddressId = 3
+WHERE [Name] LIKE '%CO%'
+
+DELETE FROM ProductsClients WHERE ClientId = 11
+DELETE FROM Invoices WHERE ClientId = 11
+DELETE FROM Clients WHERE SUBSTRING(NumberVat, 1, 2) = 'IT'
