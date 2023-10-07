3b4351f3e43563b9b86647c38f5048254e8e3985
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-10-07 22:20:27 +0100

    add queries

---
+++ EXAM PREP/Databases MSSQL Server Retake Exam - 10 Dec 2021/DDL AND DML.sql
@@ -0,0 +1,74 @@
+CREATE TABLE Passengers
+(
+	Id INT PRIMARY KEY IDENTITY,
+	FullName VARCHAR(100) UNIQUE NOT NULL,
+	Email VARCHAR(50) UNIQUE NOT NULL
+)
+
+CREATE TABLE Pilots
+(
+	Id INT PRIMARY KEY IDENTITY,
+	FirstName VARCHAR(30) UNIQUE NOT NULL,
+	LastName VARCHAR(30) UNIQUE NOT NULL,
+	Age TINYINT CHECK(Age >= 21 AND Age <= 62) NOT NULL,
+	Rating FLOAT CHECK(Rating >= 0.0 AND Rating <= 10.0)
+)
+
+CREATE TABLE AircraftTypes
+(
+	Id INT PRIMARY KEY IDENTITY,
+	TypeName VARCHAR(30) UNIQUE NOT NULL
+)
+
+CREATE TABLE Aircraft
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Manufacturer VARCHAR(25) NOT NULL,
+	Model VARCHAR(30) NOT NULL,
+	[Year] INT NOT NULL,
+	FlightHours INT,
+	Condition CHAR(1) NOT NULL,
+	TypeId INT FOREIGN KEY REFERENCES AircraftTypes(Id) NOT NULL
+)
+
+CREATE TABLE PilotsAircraft
+(
+	AircraftId INT FOREIGN KEY REFERENCES Aircraft(Id) NOT NULL,
+	PilotId INT FOREIGN KEY REFERENCES Pilots(Id) NOT NULL,
+	PRIMARY KEY(AircraftId,PilotId)
+)
+
+CREATE TABLE Airports
+(
+	Id INT PRIMARY KEY IDENTITY,
+	AirportName VARCHAR(70) UNIQUE NOT NULL,
+	Country VARCHAR(100) UNIQUE NOT NULL
+)
+
+CREATE TABLE FlightDestinations
+(
+	Id INT PRIMARY KEY IDENTITY,
+	AirportId INT FOREIGN KEY REFERENCES Airports(Id) NOT NULL,
+	[Start] DATETIME NOT NULL,
+	AircraftId INT FOREIGN KEY REFERENCES Aircraft(Id) NOT NULL,
+	PassengerId INT FOREIGN KEY REFERENCES Passengers(Id) NOT NULL,
+	TicketPrice DECIMAL DEFAULT 15 NOT NULL
+)
+
+--Insert
+INSERT INTO Passengers (FullName, Email)
+SELECT FirstName + ' ' + LastName AS FullName, 
+       REPLACE(FirstName + LastName, ' ', '') + '@gmail.com' AS Email
+FROM Pilots
+WHERE Id BETWEEN 5 AND 15;
+
+--Update
+UPDATE Aircraft
+SET Condition = 'A'
+WHERE (Condition = 'C' OR Condition = 'B')
+AND (FlightHours IS NULL OR FlightHours <= 100)
+AND ([Year] >= 2013)
+
+--Delete
+DELETE FROM Passengers 
+WHERE LEN(FullName) <= 10
